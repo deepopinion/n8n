b4b873f02d6858c2a38f29982d950e502b6e2bf3
leonqb <leon.quattlebaum@squer.io>
2025-07-23 18:05:13 +0200

    feat(hook): enhance transaction handling and add external configuration options

--- hooks/hook.js
+++ hooks/hook.js
@@ -3,20 +3,26 @@ const fetch = require('node-fetch');
 const dataLayerUrl = process.env.DATA_LAYER_BASE_URL;
 const controlHubUrl = process.env.CONTROL_HUB_BASE_URL;
 
+const executionTransactionMap = new Map();
+
 async function createTransaction(body) {
 	const url = `${dataLayerUrl}/transactions`;
-	console.log(`FETCH ${url}\n`, body);
 
-	await fetch(url, {
+	const res = await fetch(url, {
 		method: 'POST',
 		headers: {'Content-Type': 'application/json'},
 		body: JSON.stringify(body)
 	});
+
+	if (!res.ok) {
+		throw new Error(`Failed to create transaction: ${res.status} ${res.statusText}`);
+	}
+
+	return await res.json();
 }
 
-async function syncOutputToDataLayer(executionId, body) {
-	const url = `${dataLayerUrl}/transactions/executions/${executionId}/workflow-steps`;
-	console.log(`FETCH ${url}\n`, body);
+async function syncOutputToDataLayer(transactionId, body) {
+	const url = `${dataLayerUrl}/transactions/${transactionId}/workflow-step`;
 
 	await fetch(url, {
 		method: 'POST',
@@ -25,46 +31,70 @@ async function syncOutputToDataLayer(executionId, body) {
 	});
 
 	return {
-		executionId,
+		executionId: transactionId,
 		test: "1234"
 	};
 }
 
-async function triggerControlHubRules(executionId) {
-	const url = `${controlHubUrl}/transactions/business-rules-execution/all-rules/for-execution/${executionId}`;
+async function triggerControlHubRules(transactionId) {
+	const url = `${controlHubUrl}/transactions/business-rules-execution/all-rules/for-transaction/${transactionId}`;
 
-	console.log(`FETCH ${url}`);
-
-	// await fetch(url, { method: 'POST' });
+	await fetch(url, { method: 'POST' });
 }
 
 function getFlattenedOutput(node) {
 	return 'output' in node.json ? node.json.output : node.json;
 }
 
+async function getBusinessAppId(workflowId) {
+	if (process.env.OVERRIDE_BUSINESS_APP_ID) {
+		return process.env.OVERRIDE_BUSINESS_APP_ID;
+	}
+	const dbWorkflow = await this.dbCollections.Workflow.findById(workflowId);
+
+	const workflowOwner = dbWorkflow.shared.find(sharedData => sharedData.role === 'workflow:owner');
+
+	return workflowOwner?.project.name;
+}
+
+async function getWorkspaceId(businessAppId) {
+	if (process.env.OVERRIDE_WORKSPACE_ID) {
+		return process.env.OVERRIDE_WORKSPACE_ID;
+	}
+	return 'default-workspace-id'; // Replace with actual logic to get workspace ID
+}
+
 module.exports = {
 	"workflow": {
 		"preExecute": [
 			async function (workflow, _, executionId) {
-				const dbWorkflow = await this.dbCollections.Workflow.findById(workflow.id);
-
-				const workflowOwner = dbWorkflow.shared.find(sharedData => sharedData.role === 'workflow:owner');
-
-				const businessAppId = workflowOwner?.project.name;
-
 				const workflowId = workflow.id;
+
+				const businessAppId = await getBusinessAppId(workflowId);
+				const workspaceId = await getWorkspaceId(businessAppId);
+
 				const nodeInfos = Object.entries(workflow.nodes ?? {}).map(([key, value]) => ({name: key, id: value.id}));
 
-				await createTransaction({
+				const transaction = await createTransaction({
 					businessAppId,
-					executionId,
-					workflowConfiguration: {
-						workflowId,
-						nodeInfos
+					workspaceId,
+					workflowData: {
+						executionId,
+						workflowConfiguration: {
+							workflowId,
+							nodeInfos
+						}
 					}
 				});
+
+				executionTransactionMap.set(executionId, transaction.id);
 			}
 		],
+		"postExecute": [
+			async function (_, __, executionId) {
+				executionTransactionMap.delete(executionId)
+			}
+		]
 	},
 	"node": {
 		"postExecute": [
@@ -78,12 +108,18 @@ module.exports = {
 				const output = itemData.map(getFlattenedOutput);
 				const executionId = executionData.executionId;
 
-				const dataLayerValue = await syncOutputToDataLayer(executionId, {
+				const transactionId = executionTransactionMap.get(executionId.toString());
+
+				if (!transactionId) {
+					throw new Error(`No transaction found for execution ID: ${executionId}`);
+				}
+
+				const dataLayerValue = await syncOutputToDataLayer(transactionId, {
 					nodeName,
 					output
 				});
 
-				await triggerControlHubRules(executionId);
+				await triggerControlHubRules(transactionId);
 
 				itemData.forEach(element => {
 					const oldValue = getFlattenedOutput(element);
@@ -95,6 +131,7 @@ module.exports = {
 						}
 					}
 				});
+
 			}
 		],
 	}
